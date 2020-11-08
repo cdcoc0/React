bda2ad379b73d567031d552b4590306cba9d93b4
velykitty <jiyunee13@gmail.com>
2020-11-08 12:25:33 +0900

    generate dynamic components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ function Fruits({fav}) {
   return <h3>I am {fav}</h3>;
 }
 
+const fruitsILike = [
+  {name: "popo"},
+  {name: "apple"},
+  {name: "orange"},
+  {name: "cherry"},
+  {name: "coconut"}
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Chopkiri!</h1>
       <Fruits fav="podo" />
+      {fruitsILike.map(dessert => 
+        <Fruits fav={dessert.name} />)}
     </div>
   );
 }
