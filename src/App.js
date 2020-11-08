9ed0a58f520cc029a2146017a143770eddc774ca
velykitty <jiyunee13@gmail.com>
2020-11-08 12:09:04 +0900

    jsx -reusable components, props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+function Fruits({fav}) {
+  return <h3>I am {fav}</h3>;
+}
 
 function App() {
   return (
     <div className="App">
-      Hello Chopkiri!
+      <h1>Hello Chopkiri!</h1>
+      <Fruits fav="podo" />
     </div>
   );
 }
