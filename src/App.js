413a2f2019c8531f8b015cb8d90aa1648b6c9d40
velykitty <jiyunee13@gmail.com>
2020-11-10 17:25:05 +0900

    practice propTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-function Fruits({fav}) {
-  return <h3>I am {fav}</h3>;
+import PropTypes from "prop-types";
+
+function Fruits({fav, rating}) {
+  return (
+  <div>
+    <h3>I am {fav}</h3>
+    <h4>{rating}/10</h4>
+  </div>
+  );
 }
 
+Fruits.propTypes = {
+  fav: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired
+};
+
 const fruitsILike = [
-  {id: 1, name: "popo"},
-  {id: 2, name: "apple"},
-  {id: 3, name: "orange"},
-  {id: 4, name: "cherry"},
-  {id: 5, name: "coconut"}
+  {id: 1, name: "popo", rating: 5},
+  {id: 2, name: "apple", rating: 3.9},
+  {id: 3, name: "orange", rating: 2.2},
+  {id: 4, name: "cherry", rating: 6},
+  {id: 5, name: "coconut", rating: 2}
 ];
 
 function renderFruit(menu) {
@@ -20,8 +32,11 @@ function App() {
     <div className="App">
       <h1>Hello Chopkiri!</h1>
       <Fruits fav="podo" />
-      {console.log(fruitsILike.map(renderFruit))}
-      {fruitsILike.map(renderFruit)}
+      {/* {console.log(fruitsILike.map(renderFruit))}
+      {fruitsILike.map(renderFruit)} */
+      fruitsILike.map(menu => (
+        <Fruits key={menu.id} fav={menu.name} rating={menu.rating} />
+      ))}
     </div>
   );
 }
