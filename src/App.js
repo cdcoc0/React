cb88700d0efe0206664f012134ef48de2f9f4a0f
velykitty <jiyunee13@gmail.com>
2020-11-12 12:17:33 +0900

    class component and state

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-function Fruits({fav, rating}) {
-  return (
-  <div>
-    <h3>I am {fav}</h3>
-    <h4>{rating}/10</h4>
-  </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Fruits.propTypes = {
-  fav: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired
-};
-
-const fruitsILike = [
-  {id: 1, name: "popo", rating: 5},
-  {id: 2, name: "apple", rating: 3.9},
-  {id: 3, name: "orange", rating: 2.2},
-  {id: 4, name: "cherry", rating: 6},
-  {id: 5, name: "coconut", rating: 2}
-];
-
-function renderFruit(menu) {
-  //console.log(menu);
-  return <Fruits key={menu.id} fav={menu.name} />;
-}
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Chopkiri!</h1>
-      <Fruits fav="podo" />
-      {/* {console.log(fruitsILike.map(renderFruit))}
-      {fruitsILike.map(renderFruit)} */
-      fruitsILike.map(menu => (
-        <Fruits key={menu.id} fav={menu.name} rating={menu.rating} />
-      ))}
-    </div>
-  );
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>class component executed {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
